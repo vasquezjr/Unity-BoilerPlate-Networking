8158367b9a682e113faa9c61c7d4099aa18d9379
Jose R Vasquez <Jose0x52Vasquez@gmail.com>
2019-01-28 15:36:33 -0800

    Working collision and to test network

--- Assets/PlayerUnit.cs
+++ Assets/PlayerUnit.cs
@@ -110,12 +110,15 @@ public class PlayerUnit : NetworkBehaviour {
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (hasAuthority)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+        }
     }
 
     [Command]
